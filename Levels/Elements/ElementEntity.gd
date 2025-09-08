13eae9f5210d735c25e87ce4359cc5f692ae5997
nilstop <nilstopgaard@gmail.com>
2025-09-08 21:07:50 +0200

    Element Entity wrapping fixes
    X start position incosistensies are not fixes

--- Levels/Elements/ElementEntity.gd
+++ Levels/Elements/ElementEntity.gd
@@ -5,26 +5,28 @@ class_name car
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var base_width = sprite_2d.texture.get_width()
 
-var speed : int = 1500
-var width_scale : float = 1
-var direction : int = -1
-var offset : int
+var speed : int
+var width_scale : float
+var direction : int
+var end_pos : Vector2
+
+var left_corner = -Global.camera_x_divide * Global.cell_size - Global.cell_size/2.0 * width_scale
+var right_corner = Global.screen_rect.x + Global.camera_x_divide * Global.cell_size + Global.cell_size/2.0 * width_scale
+
 
 func _ready() -> void:
-	#global_position.x = Global.screen_rect.x + Global.camera_x_divide * Global.cell_size + base_width/2.0 * width_scale
+	if width_scale == 1.1:
+		print(global_position.x)
 	sprite_2d.scale.x = width_scale
 	collision_shape.scale.x = width_scale
-	visible_notif.scale.x *= width_scale
-	#if direction == 1:
-		#global_position.x = -Global.camera_x_divide * Global.cell_size - base_width/2.0 * width_scale
 
 func _process(delta: float) -> void:
-	global_position.x += direction * speed * delta
+	global_position.x += direction * speed * delta * 0.5
+	
 	if direction == -1:
-		if global_position.x < -Global.camera_x_divide * Global.cell_size - base_width/2.0 * width_scale:
-			global_position.x = Global.screen_rect.x + Global.camera_x_divide * Global.cell_size + base_width/2.0 * width_scale
+		if global_position.x < end_pos.x:
+			global_position.x = right_corner
 	else:
-		if global_position.x > Global.screen_rect.x + Global.camera_x_divide * Global.cell_size + base_width/2.0 * width_scale:
-			global_position.x = -Global.camera_x_divide * Global.cell_size - base_width/2.0 * width_scale
+		if global_position.x > end_pos.x:
+			global_position.x = left_corner
--- Levels/Elements/element.gd
+++ Levels/Elements/element.gd
@@ -17,31 +17,33 @@ extends Node2D
 @export var direction : int
 
 var inst_pos : Vector2
+var inst_positions : Array = []
 
-var left_corner = -Global.camera_x_divide * Global.cell_size + Global.cell_size/2.0 * width_scale
-var right_corner = Global.screen_rect.x - Global.camera_x_divide * Global.cell_size + Global.cell_size/2.0 * width_scale
+var left_corner = -Global.camera_x_divide * Global.cell_size - Global.cell_size/2.0 * width_scale
+var right_corner = Global.screen_rect.x + Global.camera_x_divide * Global.cell_size + Global.cell_size/2.0 * width_scale
+
 
 func _ready() -> void:
 	if direction == -1:
 		inst_pos.x = right_corner
 		while inst_pos.x > left_corner:
-			instantiate(inst_pos)
-			inst_pos.x -= frequency_seconds / Engine.get_frames_per_second() * speed
-			print(str(frequency_seconds / Engine.get_frames_per_second() * speed))
+			inst_positions.append(inst_pos)
+			inst_pos.x -= frequency_seconds * speed
 	else:
 		inst_pos.x = left_corner
 		while inst_pos.x < right_corner:
-			instantiate(inst_pos)
-			inst_pos.x += frequency_seconds / Engine.get_frames_per_second() * speed
-			print(str(frequency_seconds / Engine.get_frames_per_second() * speed))
-		
+			inst_positions.append(inst_pos)
+			inst_pos.x += frequency_seconds * speed
+	instantiate(inst_pos)
 	#set position using the exported cell variable, which is how far in the level the element is
 	global_position = Vector2(Global.screen_rect.x/2.0, Global.screen_rect.y - cell * Global.cell_size + Global.cell_size / 2.0)
 
-func instantiate(instance_position):
-	var instance = entity.instantiate()
-	instance.global_position = instance_position
-	instance.speed = speed
-	instance.width_scale = width_scale
-	instance.direction = direction
-	add_child(instance)
+func instantiate(end_position):
+	for n in inst_positions.size():
+		var instance = entity.instantiate()
+		instance.end_pos = end_position
+		instance.global_position = inst_positions.get(n)
+		instance.speed = speed
+		instance.width_scale = width_scale
+		instance.direction = direction
+		add_child(instance)
